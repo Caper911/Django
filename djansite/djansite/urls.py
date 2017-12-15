ba62d3fd032a2ea56e248599035faa7131a0482d
root <root@localhost.localdomain>
2017-12-15 04:32:02 -0500

    second push

--- djansite/djansite/urls.py
+++ djansite/djansite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from bearing import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^index$', views.index ,name ='home'),
 ]
